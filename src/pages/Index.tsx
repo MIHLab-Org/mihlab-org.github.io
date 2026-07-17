9cf5f49c06f11bb9dc5f82b8f07e39c16e051e28
redapemusic35 <redapemusic35@gmail.com>
2026-07-17 10:13:12 -0400

    add

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router';
 import { Layout } from '@/components/Layout';
 import { ArrowRight, Sparkles, Gamepad2, Music, Palette } from 'lucide-react';
-import heroSpaceStation from '@/assets/uploads/red-ship-hangar.jpg';
+import heroSpaceStation from '@/assets/uploads/mihlab-logo-on-earth.svg';
 import futuristicCity from '@/assets/uploads/472496.jpg';
 
 const researchAreas = [
